e46b97925bac98ed4858f0b2c9633708c97d44ba
Anett David <davidanett@gmail.com>
2020-05-24 10:45:20 +0200

    Post image upload

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,6 +17,7 @@ class PostType extends AbstractType
             ->add('body', TextareaType::class, [
                 'label' => 'Envoyer un nouveau publication'
             ])
+            ->add('imageFile', FileType::class, ['required' => false])
         ;
     }
 
