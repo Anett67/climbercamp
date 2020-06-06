656b098ad44ac1bfba435d7ca834296ccf2124e4
Anett David <davidanett@gmail.com>
2020-06-06 12:42:57 +0200

    Sending Messages

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
 use App\Entity\User;
+use App\Form\MessageType;
 use App\Repository\UserRepository;
 use App\Repository\MessageRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -53,12 +56,61 @@ class MessageController extends AbstractController
 
         $user = $this->getUser();
 
-        $messages = $repository->findConversation($user, $partner);
+        $messages = [];
+
+        $allMessages = $repository->findUsersMessages($user);
+
+        foreach($allMessages as $message){
+            $fromUserId = $message->getFromUser()->getId();
+            $toUserId = $message->getToUser()->getId();
+            if($fromUserId === $partner->getId() || $toUserId === $partner->getId()){
+                $messages[] = $message;
+                if($toUserId === $user->getId() && $fromUserId === $partner->getId()){
+                    $message->setSeen(true);
+                    $manager->persist($message);
+                    $manager->flush();
+                }
+            }
+        }
+
+        $newMessage = new Message();
+
+        $form = $this->createForm(MessageType::class, $newMessage);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $newMessage->setFromUser($user)
+                        ->setToUser($partner)
+                        ->setSendDate(new DateTime('now'))
+                        ->setSeen(false);
+            $manager->persist($newMessage);
+            $manager->flush();
+
+            $this->addFlash('success', 'Votre message a été envoyé avec succès');
+
+            return $this->redirectToRoute('conversation', ['id' => $partner->getId()]);
+        }
 
         return $this->render('message/conversation.html.twig',[
             'partner' => $partner,
-            'messages' => $messages
+            'messages' => $messages,
+            'form' => $form->createView()
         ]);
 
      }
+
+    /**
+    * @Route("/message/seen/{id}", name="message-seen")
+    */
+
+    public function setMessageSeen(Message $message, EntityManagerInterface $manager){
+
+        $message->setSeen(true);
+        $manager->persist($message);
+        $manager->flush();
+
+        return $this->redirectToRoute('messages');
+
+    }
 }
--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Message;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +13,9 @@ class MessageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('sendDate')
-            ->add('body')
-            ->add('seen')
-            ->add('fromUser')
-            ->add('toUser')
+            ->add('body', TextareaType::class,[
+                'label' => false
+            ])
         ;
     }
 
