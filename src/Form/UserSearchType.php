1e577ec49d0756c90734ce8db01ee7e7d4cc3f31
Anett David <davidanett@gmail.com>
2020-07-07 17:38:22 +0200

    Modication of Users Template

--- src/Form/UserSearchType.php
+++ src/Form/UserSearchType.php
@@ -2,9 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Ville;
 use App\Entity\UserSearch;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserSearchType extends AbstractType
@@ -12,9 +15,20 @@ class UserSearchType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('ville')
+            ->add('firstName', TextType::class, [
+                'label' => false,
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Prénom'
+                ]    
+            ])
+            ->add('lastName', TextType::class, [ 
+                'label' => false,
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Nom'
+                ]
+            ])
         ;
     }
 
