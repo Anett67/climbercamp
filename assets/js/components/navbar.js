027c1f8848a4542c693508eeb3b3e0abfe9457c2
Anett David <davidanett@gmail.com>
2020-07-21 09:06:05 +0200

    Add comments to JS

--- assets/js/components/navbar.js
+++ assets/js/components/navbar.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 
+//Shos the menu when the burger icon is clicked
 $('body').on('click', '.burger',function(){
     $('.menu-block').toggleClass('menu-mobile');
     $('.menu-list').toggle();
--- assets/js/components/update.js
+++ assets/js/components/update.js
@@ -1,45 +1,58 @@
 import $ from 'jquery';
 
-$(function(){
+//Load forms to update posts, events and responses with AJAX
 
-    $('body').on('click', '.update-link', function(event){
-        event.preventDefault();
-        var postBlock = $(this).closest('.update-block');
-        var url = $(this).attr('href');
+$('body').on('click', '.update-link', function(event){
+    event.preventDefault();
+
+    //The container where we want to insert the form
+    var postBlock = $(this).closest('.update-block');
+    //AJAX endpoint url
+    var url = $(this).attr('href');
+
+    //show spinner
+    postBlock.html('<div class="spinner-front"></div>');
+    postBlock.addClass('green-spinner');
     
-        postBlock.html('<div class="spinner-front"></div>');
-        postBlock.addClass('green-spinner');
-        
-        $.post(
-            url,
-            {},
-            function(response){
-                postBlock.removeClass('green-spinner');
-                postBlock.html(response.response).slideDown();
-            }
-        )
-        
-    });
-
-    $('body').on('click', '.cancel', function(){
-        var url = $(this).data('url');
-        if ($(this).hasClass('reply-update-cancel')){
-            var postBlock = $(this).parent().parent('.update-block');
-        }else{
-            var postBlock = $(this).parent('.update-block');
+    $.post(
+        url,
+        {},
+        function(response){
+            //hide spinner
+            postBlock.removeClass('green-spinner');
+            //Load the template of the update form returned by the AJAX endpoint
+            postBlock.html(response.response).slideDown();
         }
-        
-        postBlock.html('<div class="spinner-front"></div>');
-        postBlock.addClass('green-spinner');
-        
-        $.post(
-            url,
-            {},
-            function(response){
-                postBlock.removeClass('green-spinner');
-                postBlock.html(response);
-            }
-        )  
-    });
+    )
+    
+});
+
+//Load back the template of the post, event or response if user clicks on the cancel button
+$('body').on('click', '.cancel', function(){
+
+    //AJAX endpoint
+    var url = $(this).data('url');
+
+    //Checks if the object to update is a comment reply
+    if ($(this).hasClass('reply-update-cancel')){
+        var postBlock = $(this).parent().parent('.update-block');
+    }else{
+        var postBlock = $(this).parent('.update-block');
+    }
+    
+    //show spinner
+    postBlock.html('<div class="spinner-front"></div>');
+    postBlock.addClass('green-spinner');
+    
+    $.post(
+        url,
+        {},
+        function(response){
+            //hide spinner
+            postBlock.removeClass('green-spinner');
+            //Load back the template of the post, event or response
+            postBlock.html(response);
+        }
+    )  
 });
 
